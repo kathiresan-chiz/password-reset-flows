8adcd4067829c7254197b782c547f2acab20fc79
kathiresan-chiz <kathirresan999@gmail.com>
2026-01-08 21:07:00 +0530

    password frontend api.js  are added

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 
-export default axios.create({
-  baseURL: "http://localhost:5000/api"
+const api = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
+  withCredentials: true
 });
+
+export default api;
