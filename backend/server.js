9634d6a369709c02921d87d170c501c55f4eb694
kathiresan-chiz <kathirresan999@gmail.com>
2026-01-09 15:46:04 +0530

    route change in server.js
    Comment out the authentication routes middleware.

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,9 @@ app.use(cors({
 }));
 app.use(express.json());
 
-app.use("/api/auth", require("./routes/authRoutes"));
+// app.use("/api/auth", require("./routes/authRoutes"));
+    await api.post("/auth/forgot-password", { email });
+
 
 app.listen(process.env.PORT, () =>
   console.log(`Server running on port ${process.env.PORT}`)
