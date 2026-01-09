53df47b4601fb2a4261b5310d3b256d2a39afdb4
kathiresan-chiz <kathirresan999@gmail.com>
2026-01-09 16:37:48 +0530

    Add headers to axios API instance

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,9 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
-  withCredentials: true
+  headers: {
+    "Content-Type": "application/json"
+  }
 });
 
 export default api;
